b517bd2641fc95277e0238bcf4210f22c610ff13
BrantKijin <upper0house@gmail.com>
2024-01-21 16:01:07 +0900

    init: 초기 샘플 데이터

---
+++ src/main/java/hello/springbatchi/SpringBatchIApplication.java
@@ -0,0 +1,13 @@
+package hello.springbatchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springbatchi/SpringBatchIApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springbatchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
